ec609cc5d5651793c782f9cf9d23652d9039c63b
igloo1505 <iglinskia44@gmail.com>
2024-07-10 11:55:32 -0500

    Added pre-commit hooks to handle json formatting and local-remote package sourcing

--- src/components/REPLACEMEcomponent.tsx
+++ src/components/REPLACEMEcomponent.tsx
@@ -1,14 +1,9 @@
-import React from 'react'
-
+import React from "react";
 
 const REPLACEME = (props: any) => {
-return (
-    <div></div>
-)
-}
+    return <div></div>;
+};
 
-
-REPLACEME.displayName = "REPLACEME"
-
+REPLACEME.displayName = "REPLACEME";
 
 export default REPLACEME;
