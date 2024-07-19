29f540a44b5c65c0dcfa00086c0324ca0848b14b
igloo1505 <iglinskia44@gmail.com>
2024-07-19 14:03:15 -0500

    Updated some more slots, this time to accommodate the settings page.

--- src/app/settings/layout.tsx
+++ src/app/settings/layout.tsx
@@ -18,7 +18,7 @@ const SettingsLayout = ({children}: SettingsLayoutProps) => {
             <div className="flex flex-col space-y-8 lg:flex-row lg:space-x-12 lg:space-y-0">
                 <aside className="-mx-4 lg:w-1/5">
                     <MultipageSidebar 
-                        items={buildData.settingsPages}
+                        items={buildData.settingPages}
                     />
                 </aside>
                 <div className="flex-1 lg:max-w-[min(1080px,100%)]">
