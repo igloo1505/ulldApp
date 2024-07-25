ae46016b93b18c2addd1ce2dca5b7fdfab64beff
igloo1505 <iglinskia44@gmail.com>
2024-07-24 23:53:05 -0500

    Update scripts

--- buildUtils/__TEMP__/runOnBuild.ts
+++ buildUtils/__TEMP__/runOnBuild.ts
@@ -4,6 +4,7 @@ import { PathKeys } from "@ulld/utilities/types"
 
 
 (async () => {
+    console.log(`We're almost done! Running the onBuild method for each plugin.`)
     for await (const f of onBuildMethods) {
        await f.func(tempPaths as Record<PathKeys, string>) 
     }
