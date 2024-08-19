5fc89a8e40cbb0aab72e2118a35e7a8db7702bcd
igloo1505 <iglinskia44@gmail.com>
2024-08-19 14:41:48 -0500

    Automatically changed versions to match patch bump

--- src/app/settings/layout.tsx
+++ src/app/settings/layout.tsx
@@ -12,7 +12,6 @@ const SettingsLayout = ({children}: SettingsLayoutProps) => {
         <div className="space-y-6 p-10 pb-16 min-h-screen-noNav">
             <div className="space-y-0.5">
                 <h2 className="text-2xl font-bold tracking-tight">Settings</h2>
-                {/* <p className="text-muted-foreground">Settings</p> */}
             </div>
             <Separator className="my-6" />
             <div className="flex flex-col space-y-8 lg:flex-row lg:space-x-12 lg:space-y-0">
